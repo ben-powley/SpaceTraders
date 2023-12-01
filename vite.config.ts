047ca94bd6ccfb0e191c5154a171d3f623252e2d
Ben Powley <ben.powley@ubisoft.com>
2023-12-01 10:38:54 +0000

    Adjust config to allow absolute paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vitest/config'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vitest/config"
+import react from "@vitejs/plugin-react-swc"
+import tsconfigPaths from "vite-tsconfig-paths"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   test: {
     environment: "jsdom",
-    setupFiles: "./vitest.setup.ts"
-  }
+    setupFiles: "./vitest.setup.ts",
+  },
 })
