a5146a9e2e4aa6aee29dd7fc1d5509f4ff05eba6
Ben Powley <ben.powley97@gmail.com>
2023-12-02 16:13:31 +0000

    Remove code that was causing too many requests

--- src/Components/System.tsx
+++ src/Components/System.tsx
@@ -1,23 +1,11 @@
 import { System } from "Types/SpaceTraderTypes"
 import Waypoint from "./Waypoint"
-import { useEffect } from "react"
-import { useAppDispatch, useAppSelector } from "Store/Hooks"
-import fetchSystem from "Store/Thunks/GetSystem"
 
 type Props = {
   system: System
 }
 
 const System = ({ system }: Props) => {
-  const dispatch = useAppDispatch()
-  const apiKey = useAppSelector((state) => state.main.spaceTraderDetails?.data.token)
-
-  useEffect(() => {
-    if (apiKey) {
-      dispatch(fetchSystem({ apiKey: apiKey, systemSymbol: system.symbol }))
-    }
-  }, [apiKey])
-
   return (
     <div className="flex gap-3 border-b-2 pb-4 border-opacity-10 border-white">
       <div className="flex flex-col items-center justify-center text-center gap-2">
