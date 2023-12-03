667506220fea5346b0fc9811ee7e9e12e05ccb12
Ben Powley <ben.powley97@gmail.com>
2023-12-03 22:32:56 +0000

    Revert unit testing changes that broke persistence.

--- src/Store/index.ts
+++ src/Store/index.ts
@@ -1,4 +1,4 @@
-import { PreloadedState, combineReducers, configureStore } from "@reduxjs/toolkit"
+import { configureStore } from "@reduxjs/toolkit"
 import mainSliceReducer from "Store/Slices/MainSlice"
 import { persistReducer, persistStore } from "redux-persist"
 import storage from "redux-persist/lib/storage"
@@ -9,30 +9,16 @@ const persistConfig = {
 }
 
 const persistedReducer = persistReducer(persistConfig, mainSliceReducer)
-const rootReducer = combineReducers({ main: persistedReducer })
 
-export const setupStore = (preloadedState?: PreloadedState<RootState>) => {
-  return configureStore({
-    reducer: rootReducer,
-    preloadedState,
-    middleware: (getDefaultMiddleware) =>
-      getDefaultMiddleware({
-        serializableCheck: false,
-      }),
-    devTools: process.env.NODE_ENV !== "production",
-  })
-}
+export const store = configureStore({
+  reducer: { main: persistedReducer },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+  devTools: process.env.NODE_ENV !== "production",
+})
 
-// export const store = configureStore({
-//   reducer: { main: persistedReducer },
-//   middleware: (getDefaultMiddleware) =>
-//     getDefaultMiddleware({
-//       serializableCheck: false,
-//     }),
-//   devTools: process.env.NODE_ENV !== "production",
-// })
-
-export type RootState = ReturnType<typeof rootReducer>
-export type AppStore = ReturnType<typeof setupStore>
-export type AppDispatch = AppStore["dispatch"]
-export const persistor = persistStore(setupStore())
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
+export const persistor = persistStore(store)
